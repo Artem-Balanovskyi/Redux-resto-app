70ce8d30daf2dbb67088409fb9ec36e889b177a6
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-11-09 21:39:21 +0200

    Cart add/remove fn added

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -2,24 +2,33 @@ import React, { Component } from 'react';
 import MenuListItem from '../menu-list-item';
 import {connect} from 'react-redux';
 import WithRestoService from '../hoc';
+import {menuLoaded, menuRequested, addedToCart} from '../../actions';
+import Spinner from '../spinner';
 
 import './menu-list.scss';
 
 class MenuList extends Component {
     componentDidMount() {
+        this.props.menuRequested();
         const {RestoService} = this.props;
         RestoService.getMenuItems()
-        .then(res => console.log(res));
+        .then(res => this.props.menuLoaded(res));
     }
 
     render() {
-        const { menuItems } = this.props;
+        const { menuItems, loading, addedToCart } = this.props;
 
+        if (loading) {
+            return <Spinner/>
+        }
         return (
             <ul className="menu__list">
                 {
                     menuItems.map(menuItem => {
-                        return <MenuListItem key={menuItem.id} menuItem={menuItem}/>
+                        return <MenuListItem 
+                        key={menuItem.id} 
+                        menuItem={menuItem}
+                        onAddToCart={() => addedToCart(menuItem.id)}/>
                     })
                 }
             </ul>
@@ -29,9 +38,16 @@ class MenuList extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        menuItems: state.menu
+        menuItems: state.menu,
+        loading: state.loading
     }
-}
+};
+
+const mapDispatchToProps = {
+    menuLoaded,
+    menuRequested,
+    addedToCart
+};
 
 
-export default WithRestoService()(connect(mapStateToProps)(MenuList));
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
